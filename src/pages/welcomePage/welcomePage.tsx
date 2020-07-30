6e2f6de77fb691327032348cb3979dc13ab2f626
Pavel Petyakin <petyakin86@mail.ru>
2020-07-30 15:34:31 +0300

    feature(WelcomePage): some changes

--- src/pages/welcomePage/welcomePage.tsx
+++ src/pages/welcomePage/welcomePage.tsx
@@ -5,16 +5,32 @@ import { getMenuIsOpen } from "reducers/main/selectors";
 import cx from "classnames";
 import { menuIsOpen } from "reducers/main/actions";
 
-export const WelcomePage = () => {
+export function WelcomePage() {
   const dispatch = useDispatch();
   const stateMenu = useSelector(getMenuIsOpen);
   const handleClick = () => {
     dispatch(menuIsOpen(!stateMenu));
   };
 
+  function sayHi(x: number, y: number): number {
+    return x + y
+  }
+  const sayBye = (arg: number | string): string => {
+    if("number" === typeof arg) {
+      return "isNumber"
+    } else {
+      return "isString"
+    }
+  }
+  const name: number[] = [123, 321]
+  console.log("SomeText:", sayHi(11, 22));
+  console.log("-- 1 --:", sayBye("ghjhgj"));
+
+
+
   return (
     <div className={cx(s.container, { [s.active]: stateMenu })} onClick={handleClick}>
-      Привет, Артем!
+      {name.map((el, i) => <div key={i}>{el}</div>)}
     </div>
   );
 };

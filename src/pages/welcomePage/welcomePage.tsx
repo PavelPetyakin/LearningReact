27e9a74f67c128785597cc56594d869f71245050
Artem <artemburinn@gmail.com>
2020-08-03 15:04:08 +0300

    feature(WelcomePage):  task2

--- src/pages/welcomePage/welcomePage.tsx
+++ src/pages/welcomePage/welcomePage.tsx
@@ -2,16 +2,42 @@ import React from "react";
 import s from "./style.scss";
 
 export function WelcomePage() {
+  const rec = (
+    <>
+      <div className={s.box}>
+        <div className={s.red}/>
+        <div className={s.text}>Красный</div>
+      </div>
+      <div className={s.box}>
+        <div className={s.orange}/>
+        <div className={s.text}>Оранжевый</div>
+      </div>
+      <div className={s.box}>
+        <div className={s.yellow}/>
+        <div className={s.text}>Жёлтый</div>
+      </div>
+      <div className={s.box}>
+        <div className={s.green}/>
+        <div className={s.text}>Зелёный</div>
+      </div>
+      <div className={s.box}>
+        <div className={s.skyblue}/>
+        <div className={s.text}>Голубой</div>
+      </div>
+      <div className={s.box}>
+        <div className={s.blue}/>
+        <div className={s.text}>Синий</div>
+      </div>
+      <div className={s.box}>
+        <div className={s.purple}/>
+        <div className={s.text}>Фиалетовый</div>
+      </div>
+    </>
+  )
 
   return (
     <div className={s.container}>
-      <div className={s.red}>Красный</div>
-      <div className={s.orange}>Оранжевый</div>
-      <div className={s.yellow}>Желтый</div>
-      <div className={s.green}>Зеленый</div>
-      <div className={s.skyblue}>Голубой</div>
-      <div className={s.blue}>Синий</div>
-      <div className={s.purple}>Фиолетовый</div>
+      {rec}
     </div>
   );
 }

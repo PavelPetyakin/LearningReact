a5eb268bc76eca32466a0cf48f2df43ad4bc977f
Pavel Petyakin <petyakin86@mail.ru>
2020-08-07 18:14:56 +0300

    feature(TaskPage): task 4

--- src/pages/taskPage/taskPage.tsx
+++ src/pages/taskPage/taskPage.tsx
@@ -1,51 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./style.scss";
-import { useSelector, useDispatch } from "react-redux";
-import { getMenuIsOpen } from "reducers/main/selectors";
-import cx from "classnames";
-import { menuIsOpen } from "reducers/main/actions";
+import { ColorBox, IColorBox } from "./components/colorBox/colorBox";
+
+const dataArray: Pick<IColorBox, "text" | "color">[] = [
+  {
+    color: "red",
+    text: "Красный",
+  },
+  {
+    color: "orange",
+    text: "Оранжевый",
+  },
+  {
+    color: "yellow",
+    text: "Жёлтый",
+  },
+  {
+    color: "green",
+    text: "Зелёный"
+  },
+  {
+    color: "skyblue",
+    text: "Голубой"
+  },
+  {
+    color: "blue",
+    text: "Синий"
+  },
+  {
+    color: "purple",
+    text: "Фиолетовый"
+  },
+  {
+    color: "darkred",
+    text: "Бордовый",
+  },
+  {
+    color: "coral",
+    text: "Кораловый",
+  },
+  {
+    color: "gold",
+    text: "Золотой",
+  },
+  {
+    color: "lightgreen",
+    text: "Светло-зеленый"
+  },
+  {
+    color: "lightblue",
+    text: "Светло-синий"
+  },
+  {
+    color: "darkblue",
+    text: "Темно-синий"
+  },
+  {
+    color: "pink",
+    text: "Розовый"
+  }
+]
 
 export function TaskPage() {
-  const dispatch = useDispatch();
-  const stateMenu = useSelector(getMenuIsOpen);
-  const handleClick = () => {
-    dispatch(menuIsOpen(!stateMenu));
+  const [state, setState] = useState<string>("");
+
+  const handleClick = (value: string) => () => {
+    if (value === state) {
+      setState("");
+    } else {
+      setState(value);
+    }
   };
 
-  const colors = <>
-    <div className={s.box}>
-      <div className={s.red}/>
-      <div className={s.colorName}>Red</div>
-    </div>
-    <div className={s.box}>
-      <div className={s.orange}/>
-      <div className={s.colorName}>Orange</div>
-    </div>
-    <div className={s.box}>
-      <div className={s.yellow}/>
-      <div className={s.colorName}>Yellow</div>
-    </div>
-    <div className={s.box}>
-      <div className={s.green}/>
-      <div className={s.colorName}>Green</div>
-    </div>
-    <div className={s.box}>
-      <div className={s.skyblue}/>
-      <div className={s.colorName}>Skyblue</div>
-    </div>
-    <div className={s.box}>
-      <div className={s.blue}/>
-      <div className={s.colorName}>Blue</div>
-    </div>
-    <div className={s.box}>
-      <div className={s.purple}/>
-      <div className={s.colorName}>Purple</div>
-    </div>
-  </>
+  const renderText = state ? <p>{state} &#128515;</p> : <p>&#128564;</p>;
+
+  const colors = dataArray.map((element: Pick<IColorBox, "text" | "color">, index: number) => {
+    return (
+      <ColorBox
+        key={index}
+        text={element.text}
+        color={element.color}
+        activeColor={state}
+        onClick={handleClick(element.text)}
+      />
+    )
+  })
 
   return (
-    <div className={cx(s.container, { [s.active]: stateMenu })} onClick={handleClick}>
-      {colors}
+    <div className={s.container}>
+      <h1 children={"Выберите цвет!"} />
+      <div className={s.colors}>{colors}</div>
+      <div className={s.text}>{renderText}</div>
     </div>
   );
 }

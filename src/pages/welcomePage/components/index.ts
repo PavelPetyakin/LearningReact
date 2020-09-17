b4cc244ac70e4cb81f9d5cf06cb5a3518701e17f
Artem <artemburinn@gmail.com>
2020-09-18 00:00:20 +0300

    refactor

---
+++ src/pages/welcomePage/components/index.ts
@@ -0,0 +1,6 @@
+export { User, IUser } from "./user/user";
+export { Balance } from "./balance/balance";
+export { Currencies } from "./currencies/currencies";
+export { ICurrency } from "./currencies/currency";
+export { Element, IElement } from "./element/element";
+export { Menu } from "./menu/menu";
--- src/pages/welcomePage/welcomePage.tsx
+++ src/pages/welcomePage/welcomePage.tsx
@@ -1,10 +1,7 @@
-import { number } from "@storybook/addon-knobs";
-import React, { useState } from "react";
-import { Button } from "./components/button/button"
-import { ColorRec, IColorRec } from "./components/colorRec/colorRec";
-import s from "./style.scss";
 import { Avatar } from "assets";
-import cx from "classnames";
+import React, { useState } from "react";
+import { Balance, Currencies, ICurrency, IElement, IUser, Menu, User } from "./components"
+import s from "./style.scss";
 
 interface IBigData {
   user: IUser;
@@ -14,6 +11,7 @@ interface IBigData {
 interface IBalanceInfo {
   balance: number;
   currencies: ICurrency[];
+  recentOperations: IElement[];
 }
 
 const data: ICurrency[] = [
@@ -35,13 +33,25 @@ const bigData: IBigData = {
   balanceInfo: {
     balance: 162.00,
     currencies: data,
+    recentOperations: [
+      {
+        title: "Grocery",
+        description: "Treasure island mall",
+        summary: 12,
+      },
+      {
+        title: "Petrol",
+        description: "Essar petrol pump",
+        summary: 9,
+      }
+    ],
   }
 }
 
 export function WelcomePage() {
   const { user, balanceInfo } = bigData;
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const handle = () => setIsOpen(!isOpen);
+  const [isClosed, setIsClosed] = useState<boolean>(false);
+  const handle = () => setIsClosed(!isClosed);
 
   return (
     <div className={s.container}>
@@ -51,92 +61,8 @@ export function WelcomePage() {
           <Balance balance={balanceInfo.balance}/>
           <Currencies currencies={balanceInfo.currencies}/>
         </div>
-        <div className={cx(s.wrap, {[s.closed]: isOpen})}>
-          <div className={s.menu}>
-            <div className={s.line} onClick={handle}/>
-            <div className={s.recent}>Recent transactions</div>
-            <div className={s.recentList}>
-              <div className={s.recentElement}>
-                <div>
-                  <div className={s.recentTitle}>Grocery</div>
-                  <div className={s.recentDescription}>Treasure island mall</div>
-                </div>
-                <div>$12.00</div>
-              </div>
-              <div className={s.recentElement}>
-                <div>
-                  <div className={s.recentTitle}>Petrol</div>
-                  <div className={s.recentDescription}>Essar petrol pump</div>
-                </div>
-                <div>$09.00</div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Menu isClosed={isClosed} handle={handle} recentOperations={balanceInfo.recentOperations}/>
       </div>
-    </div>
-  )
-}
-
-interface IUser {
-  avatarUrl: string;
-  name: string;
-}
-
-function User(props: IUser) {
-  const { avatarUrl, name } = props;
-
-  return (
-    <div className={s.user}>
-      <img className={s.userAvatar} src={avatarUrl}/>
-      <div className={s.userName}>{name}</div>
-    </div>
-  )
-}
-
-interface IBalance {
-  balance: number;
-}
-
-function Balance(props: IBalance) {
-  const amount: string = "$" + props.balance + ".00";
-
-  return (
-    <div className={s.balance}>
-      <div className={s.balanceTitle}>Your total balance is</div>
-      <div className={s.balanceAmount}>{amount}</div>
-    </div>
-  )
-}
-
-interface ICurrency {
-  amount: number;
-  type: string;
-}
-
-function Currency(props: ICurrency) {
-  const { amount, type } = props;
-
-  return (
-    <div className={s.currency}>
-      <div className={s.currencyAmount}>{`$${amount}.00`}</div>
-      <div className={s.currencyType}>{type}</div>
-    </div>
-  )
-}
-
-interface ICurrencies {
-  currencies: ICurrency[];
-}
-
-function Currencies(props: ICurrencies) {
-  const currencies = props.currencies.map((elem, index) => {
-    return <Currency key={index} amount={elem.amount} type={elem.type}/>
-  })
-
-  return (
-    <div className={s.currencies}>
-      {currencies}
     </div>
   )
 }

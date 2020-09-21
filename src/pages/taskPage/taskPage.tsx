aa13f691882585be555470374ce3092e226eb5df
Artem <artemburinn@gmail.com>
2020-09-21 22:48:27 +0300

    feature: create submetric element my version

--- src/pages/taskPage/taskPage.tsx
+++ src/pages/taskPage/taskPage.tsx
@@ -8,10 +8,13 @@ export function TaskPage() {
     <div className={s.container}>
       <div className={s.submetric}>
         <div className={s.title}>
-          <h1>Увлеченность работой</h1>
+          <div className={s.text}>Увлечённость работой</div>
           <Icon.Cross className={s.cross}/>
         </div>
-        <div className={s.list}>второй</div>
+        <div className={s.list}>
+          <div className={s.frame}>Вовлечённость</div>
+          <div className={s.frame}>Интерес к содержанию работы</div>
+        </div>
         <button className={s.button}>
           <Icon.Add className={s.add}/>
           <div>Добавить субметрику</div>

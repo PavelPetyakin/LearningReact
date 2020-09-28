0754b01209d51745e127123487f2cd00bdd7dd49
Artem <artemburinn@gmail.com>
2020-09-28 22:34:39 +0300

    feature: create submetric increment

--- src/pages/taskPage/taskPage.tsx
+++ src/pages/taskPage/taskPage.tsx
@@ -2,7 +2,31 @@ import { Icon } from "assets/svg";
 import React, { useState } from "react";
 import s from "./style.scss";
 
+const array: string[] = [
+  "Вовлечённость",
+  "Интерес к содержанию работы Интерес к содержанию работы Интерес к содержанию работы "
+]
+
+
 export function TaskPage() {
+  const [data, updateData] = useState<string[]>(array);
+  const [num, setNam] = useState<number>(0);
+
+  const handleIncrement = ()  => {
+    setNam(num + 1);
+  }
+
+  const handleAdd = () => {
+    const arr = [...data];
+    arr.push("Привет");
+    updateData(arr);
+  }
+
+  const deleteElement = (orderParam) => () => {
+    const newArr = [...data];
+    newArr.splice(orderParam, 1);
+    updateData(newArr);
+  }
 
   return (
     <div className={s.container}>
@@ -12,12 +36,22 @@ export function TaskPage() {
           <Icon.Cross className={s.cross}/>
         </div>
         <div className={s.list}>
-          <div className={s.frame}>Вовлечённость</div>
-          <div className={s.frame}>Интерес к содержанию работы</div>
+          {data.map((item, i) => {
+            console.log(i);
+            return (
+              <div className={s.frame} key={i}>
+                <span>{item}</span>
+                <Icon.Cross className={s.cross} onClick={deleteElement(i)}/>
+              </div>
+            )
+          })}
         </div>
-        <button className={s.button}>
+        <button className={s.button} onClick={handleAdd}>
           <Icon.Add className={s.add}/>
           <div>Добавить субметрику</div>
+        </button>
+        <button className={s.button} onClick={handleIncrement}>
+          <div>Нажал на кнопку: {num}</div>
         </button>
       </div>
     </div>
